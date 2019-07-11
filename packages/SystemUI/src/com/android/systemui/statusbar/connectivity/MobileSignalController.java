18f2abd5daf7c99dcd043e92731ab1c1efd99c9f
varund7726 <date.varun123@gmail.com>
2019-07-11 11:04:15 +0300

    SystemUI: Option to disable Data Disabled Indicator icon [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -85,6 +85,8 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
 
     private static final String ROAMING_INDICATOR_ICON =
             "system:" + Settings.System.ROAMING_INDICATOR_ICON;
+    private static final String DATA_DISABLED_ICON =
+            "system:" + Settings.System.DATA_DISABLED_ICON;
 
     private final TelephonyManager mPhone;
     private final CarrierConfigTracker mCarrierConfigTracker;
@@ -119,6 +121,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
 
     private boolean mIsVowifiAvailable;
     private boolean mRoamingIconAllowed;
+    private boolean mDataDisabledIcon;
 
     private final MobileStatusTracker.Callback mMobileCallback =
             new MobileStatusTracker.Callback() {
@@ -258,6 +261,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         mProviderModelBehavior = SystemProperties.getBoolean("persist.sys.flags.combined_signal_icons", true);
 
         Dependency.get(TunerService.class).addTunable(this, ROAMING_INDICATOR_ICON);
+        Dependency.get(TunerService.class).addTunable(this, DATA_DISABLED_ICON);
     }
 
     @Override
@@ -265,6 +269,11 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         switch (key) {
             case ROAMING_INDICATOR_ICON:
                 mRoamingIconAllowed =
+                    TunerService.parseIntegerSwitch(newValue, true);
+                updateTelephony();
+                break;
+            case DATA_DISABLED_ICON:
+                mDataDisabledIcon = 
                     TunerService.parseIntegerSwitch(newValue, true);
                 updateTelephony();
                 break;
@@ -859,7 +868,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         mCurrentState.roaming = isRoaming() && mRoamingIconAllowed;
         if (isCarrierNetworkChangeActive()) {
             mCurrentState.iconGroup = TelephonyIcons.CARRIER_NETWORK_CHANGE;
-        } else if (isDataDisabled() && !mConfig.alwaysShowDataRatIcon) {
+        } else if (isDataDisabled() && mDataDisabledIcon) {
             if (mSubscriptionInfo.getSubscriptionId() != mDefaults.getDefaultDataSubId()) {
                 mCurrentState.iconGroup = TelephonyIcons.NOT_DEFAULT_DATA;
             } else {
